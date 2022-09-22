fd304f013d9b7028df265d4d4d665074c9ecab7d
Phyrone <phyrone@phyrone.de>
2022-09-22 12:10:56 +0000

    Deploying to gh-pages from @ Phyrone/zwie@6974485edcb8762bd8ed4a89b7496ef1e1c1285a 🚀

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
 const _ = [
   "/_app/immutable/assets/fa-solid-900-d27bc752.woff2",
   "/_app/immutable/assets/fa-solid-900-6d53c706.ttf",
-  "/_app/immutable/start-4e21e11d.js",
+  "/_app/immutable/start-188278a2.js",
   "/_app/immutable/components/pages/_layout.svelte-2c49f207.js",
   "/_app/immutable/assets/_layout-78e78c70.css",
-  "/_app/immutable/components/error.svelte-d4cfa25c.js",
+  "/_app/immutable/components/error.svelte-8cd8ba51.js",
   "/_app/immutable/components/pages/(main)/_layout.svelte-1183d5fe.js",
   "/_app/immutable/components/pages/(main)/s/_server_/_layout.svelte-02e387ad.js",
   "/_app/immutable/components/pages/settings/_layout.svelte-84d91536.js",
@@ -14,14 +14,14 @@ const _ = [
   "/_app/immutable/components/pages/settings/_page.svelte-9347d66a.js",
   "/_app/immutable/modules/pages/_layout.ts-d347cb9b.js",
   "/_app/immutable/modules/pages/(main)/_page.ts-a6b9ddf0.js",
-  "/_app/immutable/chunks/singletons-8ed6720e.js",
+  "/_app/immutable/chunks/singletons-acc29451.js",
   "/_app/immutable/chunks/index-1eb7d44b.js",
   "/_app/immutable/chunks/index-d12f5de2.js",
   "/_app/immutable/chunks/sweetalert2.all-0fd2fd5e.js",
   "/_app/immutable/chunks/_layout-d02ecff8.js",
   "/_app/immutable/chunks/_page-9468531c.js",
   "/_app/immutable/chunks/0-785abb6f.js",
-  "/_app/immutable/chunks/1-c24c221c.js",
+  "/_app/immutable/chunks/1-1a080627.js",
   "/_app/immutable/chunks/2-9dd3f7b6.js",
   "/_app/immutable/chunks/3-ca9234c4.js",
   "/_app/immutable/chunks/4-9e67cbbf.js",
@@ -29,15 +29,15 @@ const _ = [
   "/_app/immutable/chunks/6-29b81c71.js",
   "/_app/immutable/chunks/7-a1ab3bd5.js",
   "/_app/immutable/chunks/8-49669905.js"
-], d = [
+], u = [
   "/app-icon-dev-rounded.png",
   "/favicon.ico",
   "/manifest.webmanifest"
-], u = [
+], d = [
   "/"
-], n = "1663847527744";
+], n = "1663848644275";
 console.log("[ServiceWorker] Init...");
-const f = [..._, ...u, ...d], i = "offline::", l = i + n;
+const f = [..._, ...d, ...u], i = "offline::", l = i + n;
 let o = caches.open(l);
 const p = self.location.host;
 console.debug("[ServiceWorker] host=" + p);
