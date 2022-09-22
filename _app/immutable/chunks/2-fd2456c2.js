1dec283f3ec097674deacafebfbd3263511ac4a1
Phyrone <phyrone@phyrone.de>
2022-09-22 10:09:07 +0000

    Deploying to gh-pages from @ Phyrone/zwie@302053f501a17baf3fb17c2417a65f5aa4b2a731 🚀

--- _app/immutable/chunks/2-fd2456c2.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/(main)/_layout.svelte-c8305d9f.js";export{t as component};
--- service-worker.js
+++ service-worker.js
@@ -1,48 +1,41 @@
 const _ = [
   "/_app/immutable/assets/fa-solid-900-d27bc752.woff2",
   "/_app/immutable/assets/fa-solid-900-6d53c706.ttf",
-  "/_app/immutable/start-d7cb9f88.js",
-  "/_app/immutable/components/pages/_layout.svelte-231fc372.js",
+  "/_app/immutable/start-f9952862.js",
+  "/_app/immutable/components/pages/_layout.svelte-2c49f207.js",
   "/_app/immutable/assets/_layout-78e78c70.css",
-  "/_app/immutable/components/error.svelte-1117d6a0.js",
-  "/_app/immutable/components/pages/(main)/_layout.svelte-c8305d9f.js",
-  "/_app/immutable/components/pages/(main)/s/_layout.svelte-70d3384f.js",
+  "/_app/immutable/components/error.svelte-bac63e1a.js",
+  "/_app/immutable/components/pages/(main)/_layout.svelte-1183d5fe.js",
   "/_app/immutable/components/pages/(main)/s/_server_/_layout.svelte-02e387ad.js",
   "/_app/immutable/components/pages/settings/_layout.svelte-84d91536.js",
   "/_app/immutable/components/pages/(main)/_page.svelte-ad6b3503.js",
   "/_app/immutable/components/pages/(main)/s/_server_/_page.svelte-3c2795c4.js",
   "/_app/immutable/components/pages/(main)/s/_server_/channel/_channel_/_page.svelte-018b3e63.js",
   "/_app/immutable/components/pages/settings/_page.svelte-9347d66a.js",
-  "/_app/immutable/modules/pages/_layout.ts-5ffbbe04.js",
-  "/_app/immutable/modules/pages/(main)/s/_layout.ts-e6dc5166.js",
-  "/_app/immutable/chunks/singletons-284459cf.js",
+  "/_app/immutable/modules/pages/_layout.ts-d347cb9b.js",
+  "/_app/immutable/chunks/singletons-8718e155.js",
   "/_app/immutable/chunks/index-1eb7d44b.js",
   "/_app/immutable/chunks/index-d12f5de2.js",
   "/_app/immutable/chunks/sweetalert2.all-0fd2fd5e.js",
-  "/_app/immutable/chunks/_layout-b711fdb6.js",
-  "/_app/immutable/chunks/_layout-d81b7549.js",
-  "/_app/immutable/chunks/0-e6478a3d.js",
-  "/_app/immutable/chunks/1-45acd031.js",
-  "/_app/immutable/chunks/2-fd2456c2.js",
-  "/_app/immutable/chunks/3-9705395e.js",
-  "/_app/immutable/chunks/4-3f2b3cba.js",
-  "/_app/immutable/chunks/5-23a8fd27.js",
-  "/_app/immutable/chunks/6-aae51bed.js",
-  "/_app/immutable/chunks/7-b5009314.js",
-  "/_app/immutable/chunks/8-cdee6e32.js",
-  "/_app/immutable/chunks/9-86b2c8fc.js"
-], u = [
+  "/_app/immutable/chunks/_layout-d02ecff8.js",
+  "/_app/immutable/chunks/0-785abb6f.js",
+  "/_app/immutable/chunks/1-fcb8a501.js",
+  "/_app/immutable/chunks/2-9dd3f7b6.js",
+  "/_app/immutable/chunks/3-ca9234c4.js",
+  "/_app/immutable/chunks/4-9e67cbbf.js",
+  "/_app/immutable/chunks/5-502fcbdd.js",
+  "/_app/immutable/chunks/6-29b81c71.js",
+  "/_app/immutable/chunks/7-a1ab3bd5.js",
+  "/_app/immutable/chunks/8-49669905.js"
+], r = [
   "/app-icon-dev-rounded.png",
   "/favicon.ico",
   "/manifest.webmanifest"
-], d = [
-  "/",
-  "/settings"
-], t = "1663838243832", r = [..._, ...d, ...u];
+], u = [], s = "1663841340357", d = [..._, ...u, ...r];
 console.log("[ServiceWorker] Init...");
-const i = "offline::", c = i + t;
+const i = "offline::", c = i + s;
 let n = caches.open(c), l;
-p();
+o();
 async function f() {
   try {
     console.time("[ServiceWorker] deleting old caches"), await Promise.all(await caches.keys().then((e) => e.filter((a) => a.startsWith(i) && a !== c)).then((e) => e.map((a) => caches.delete(a))));
@@ -51,27 +44,27 @@ async function f() {
   }
 }
 async function b() {
-  console.time(`[ServiceWorker] Creating/Updating Cache... (${t})`);
+  console.time(`[ServiceWorker] Creating/Updating Cache... (${s})`);
   let e = -1;
   try {
     let a = await n;
-    e = await Promise.all(r.map((s) => a.add(s))).then((s) => s.length), await p();
+    e = await Promise.all(d.map((t) => a.add(t))).then((t) => t.length), await o();
   } finally {
-    console.timeEnd(`[ServiceWorker] Creating/Updating Cache... (${t})`);
+    console.timeEnd(`[ServiceWorker] Creating/Updating Cache... (${s})`);
   }
   console.log(`[ServiceWorker] Added ${e} files to cache`);
 }
-async function p() {
+async function o() {
   let e = await n;
-  l = await Promise.all(await e.keys().then((a) => a.map((s) => s.url)));
+  l = await Promise.all(await e.keys().then((a) => a.map((t) => t.url)));
 }
 async function h(e) {
-  let a = await n, s = await a.match(e);
-  if (s)
-    return s;
+  let a = await n, t = await a.match(e);
+  if (t)
+    return t;
   {
-    let m = a.add(e), o = fetch(e);
-    return await m, await o;
+    let p = a.add(e), m = fetch(e);
+    return await p, await m;
   }
 }
 addEventListener("install", (e) => {
