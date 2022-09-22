690dd2dd8f28c5672fb6f5880bf11ba2ffc1c64b
Phyrone <phyrone@phyrone.de>
2022-09-22 14:05:57 +0000

    Deploying to gh-pages from @ Phyrone/zwie@7e2f94090ef1f827c597c752e6efa29d40f1a71d 🚀

---
+++ _app/immutable/chunks/1-d13c4d33.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-f72a496c.js";export{t as component};
--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const d = [
+const u = [
   "/_app/immutable/assets/fa-solid-900-d27bc752.woff2",
   "/_app/immutable/assets/fa-solid-900-6d53c706.ttf",
-  "/_app/immutable/start-e3443c7b.js",
+  "/_app/immutable/start-57635b82.js",
   "/_app/immutable/components/pages/_layout.svelte-2c49f207.js",
   "/_app/immutable/assets/_layout-78e78c70.css",
-  "/_app/immutable/components/error.svelte-2ba47bc6.js",
+  "/_app/immutable/components/error.svelte-f72a496c.js",
   "/_app/immutable/components/pages/(main)/_layout.svelte-1183d5fe.js",
   "/_app/immutable/components/pages/(main)/s/_server_/_layout.svelte-02e387ad.js",
   "/_app/immutable/components/pages/settings/_layout.svelte-84d91536.js",
@@ -14,14 +14,14 @@ const d = [
   "/_app/immutable/components/pages/settings/_page.svelte-9347d66a.js",
   "/_app/immutable/modules/pages/_layout.ts-d347cb9b.js",
   "/_app/immutable/modules/pages/(main)/_page.ts-a6b9ddf0.js",
-  "/_app/immutable/chunks/singletons-fde810e3.js",
+  "/_app/immutable/chunks/singletons-542c093e.js",
   "/_app/immutable/chunks/index-1eb7d44b.js",
   "/_app/immutable/chunks/index-d12f5de2.js",
   "/_app/immutable/chunks/sweetalert2.all-0fd2fd5e.js",
   "/_app/immutable/chunks/_layout-d02ecff8.js",
   "/_app/immutable/chunks/_page-9468531c.js",
   "/_app/immutable/chunks/0-785abb6f.js",
-  "/_app/immutable/chunks/1-2da875f7.js",
+  "/_app/immutable/chunks/1-d13c4d33.js",
   "/_app/immutable/chunks/2-9dd3f7b6.js",
   "/_app/immutable/chunks/3-ca9234c4.js",
   "/_app/immutable/chunks/4-9e67cbbf.js",
@@ -29,21 +29,21 @@ const d = [
   "/_app/immutable/chunks/6-29b81c71.js",
   "/_app/immutable/chunks/7-a1ab3bd5.js",
   "/_app/immutable/chunks/8-49669905.js"
-], u = [
+], d = [
   "/app-icon-dev-rounded.png",
   "/favicon.ico",
   "/manifest.webmanifest"
-], f = [
+], h = [
   "/"
-], n = "1663855060529";
+], n = "1663855549867";
 console.log("[ServiceWorker] Init...");
-const h = [...d, ...f, ...u], i = "offline::", l = i + n;
-let o = caches.open(l);
-const p = self.location.host;
-console.debug("[ServiceWorker] host=" + p);
+const f = [...u, ...h, ...d], l = "offline::", o = l + n;
+let p = caches.open(o);
+const m = self.location.host;
+console.debug("[ServiceWorker] host=" + m);
 async function b() {
   try {
-    console.time("[ServiceWorker] deleting old caches"), await Promise.all(await caches.keys().then((e) => e.filter((a) => a.startsWith(i) && a !== l)).then((e) => e.map((a) => caches.delete(a))));
+    console.time("[ServiceWorker] deleting old caches"), await Promise.all(await caches.keys().then((e) => e.filter((a) => a.startsWith(l) && a !== o)).then((e) => e.map((a) => caches.delete(a))));
   } finally {
     console.timeEnd("[ServiceWorker] deleting old caches");
   }
@@ -52,10 +52,10 @@ async function g() {
   console.time(`[ServiceWorker] Creating/Updating Cache... (${n})`);
   let e = -1;
   try {
-    let a = await o;
-    e = await Promise.all(h.map((t) => {
-      a.add(t).catch((s) => {
-        console.error(`[ServiceWorker] Failed to cache ${t}`, s);
+    let a = await p;
+    e = await Promise.all(f.map((t) => {
+      a.add(t).catch((c) => {
+        console.error(`[ServiceWorker] Failed to cache ${t}`, c);
       });
     })).then((t) => t.length);
   } finally {
@@ -64,19 +64,23 @@ async function g() {
   console.log(`[ServiceWorker] Added ${e} files to cache`);
 }
 async function v(e) {
-  var s;
-  let a = await o, t = await a.match(e);
+  var c, i;
+  let a = await p, t = await a.match(e);
   if (t)
     return t;
   {
-    let m = a.add(e), r = fetch(e);
+    let r = a.add(e), _ = fetch(e);
     try {
-      await m;
-    } catch (_) {
-      console.error(`[ServiceWorker] Failed to cache ${e.url}`, _);
+      await r;
+    } catch (s) {
+      console.error(`[ServiceWorker] Failed to cache ${e.url}`, s);
     }
-    let c = await r;
-    return c.ok ? c : (s = await a.match("/")) != null ? s : c;
+    try {
+      let s = await _;
+      return s.ok ? s : (c = await a.match("/")) != null ? c : s;
+    } catch {
+      return (i = await a.match("/")) != null ? i : Response.error();
+    }
   }
 }
 addEventListener("install", (e) => {
@@ -88,7 +92,7 @@ addEventListener("activate", (e) => {
 addEventListener(
   "fetch",
   async (e) => {
-    new URL(e.request.url).host === p && e.respondWith(v(e.request));
+    new URL(e.request.url).host === m && e.respondWith(v(e.request));
   }
 );
 self.addEventListener("message", (e) => {
