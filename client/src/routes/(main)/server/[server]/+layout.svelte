439f1eff24c273995b969bfb6a77b5f9a9401477
Phyrone <phyrone@phyrone.de>
2022-09-27 09:40:30 +0200

    repaced server layout with theme respecting colors

--- client/src/routes/(main)/server/[server]/+layout.svelte
+++ client/src/routes/(main)/server/[server]/+layout.svelte
@@ -11,13 +11,13 @@
 
 </script>
 
-<div class="bg-base-300 flex flex-row h-full bg-base-100">
-    <div class="bg-base-300 flex flex-col overflow-y-auto bg-gray-800">
+<div class="flex flex-row h-full bg-base-300">
+    <div class=" flex flex-col overflow-y-auto bg-base-200/75">
         <button class="btn ">Channel 1</button>
         <button class="btn ">Channel 2</button>
         <button class="btn ">Channel 3</button>
     </div>
-    <div class="flex flex-auto h-full bg-gray-700">
+    <div class="flex flex-auto h-full bg-base-200">
         <slot/>
     </div>
 </div>
