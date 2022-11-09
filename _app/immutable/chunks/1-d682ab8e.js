fbd53d5e61a8d192fea205ef87834a3c83c247f0
Phyrone <phyrone@phyrone.de>
2022-11-09 18:53:03 +0000

    Deploying to gh-pages from @ Phyrone/zwie@bb1ec7d45bb928227784f6be7367e965b9919560 🚀

--- _app/immutable/chunks/1-d682ab8e.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-d9e2687f.js";export{t as component};
--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
 const o = [
   "/_app/immutable/assets/fa-solid-900-d27bc752.woff2",
   "/_app/immutable/assets/fa-solid-900-6d53c706.ttf",
-  "/_app/immutable/start-49e409bf.js",
+  "/_app/immutable/start-b73ecdee.js",
   "/_app/immutable/components/pages/_layout.svelte-2364d8bc.js",
   "/_app/immutable/assets/_layout-8bdc1d19.css",
-  "/_app/immutable/components/error.svelte-d9e2687f.js",
+  "/_app/immutable/components/error.svelte-8797c5f2.js",
   "/_app/immutable/components/pages/(main)/_layout.svelte-61d2cf65.js",
   "/_app/immutable/components/pages/(main)/plugins/_layout.svelte-17450b86.js",
   "/_app/immutable/components/pages/(main)/server/_server_/_layout.svelte-b73916d2.js",
@@ -26,7 +26,7 @@ const o = [
   "/_app/immutable/components/pages/(main)/toolbox/_page.svelte-769cfce9.js",
   "/_app/immutable/modules/pages/_layout.ts-d347cb9b.js",
   "/_app/immutable/modules/pages/(main)/_page.ts-a6b9ddf0.js",
-  "/_app/immutable/chunks/singletons-eaf66ab7.js",
+  "/_app/immutable/chunks/singletons-cf03b962.js",
   "/_app/immutable/chunks/index-46970648.js",
   "/_app/immutable/chunks/index-bc01506a.js",
   "/_app/immutable/chunks/preload-helper-b21cceae.js",
@@ -36,7 +36,7 @@ const o = [
   "/_app/immutable/chunks/_layout-d02ecff8.js",
   "/_app/immutable/chunks/_page-9468531c.js",
   "/_app/immutable/chunks/0-17349a60.js",
-  "/_app/immutable/chunks/1-d682ab8e.js",
+  "/_app/immutable/chunks/1-8cb5cd80.js",
   "/_app/immutable/chunks/2-211d429a.js",
   "/_app/immutable/chunks/3-8ac24845.js",
   "/_app/immutable/chunks/4-1bc2b554.js",
@@ -64,15 +64,15 @@ const o = [
   "/manifest.webmanifest"
 ], _ = [
   "/"
-], n = "1668019401193";
+], n = "1668019972483";
 console.log("[ServiceWorker] Init...");
-const b = [...o, ..._, ...u], m = "offline::", c = m + n;
-let i = caches.open(c);
+const b = [...o, ..._, ...u], c = "offline::", m = c + n;
+let i = caches.open(m);
 const l = self.location.host;
 console.debug("[ServiceWorker] host=" + l);
 async function r() {
   try {
-    console.time("[ServiceWorker] deleting old caches"), await Promise.all(await caches.keys().then((e) => e.filter((a) => a.startsWith(m) && a !== c)).then((e) => e.map((a) => caches.delete(a))));
+    console.time("[ServiceWorker] deleting old caches"), await Promise.all(await caches.keys().then((e) => e.filter((a) => a.startsWith(c) && a !== m)).then((e) => e.map((a) => caches.delete(a))));
   } finally {
     console.timeEnd("[ServiceWorker] deleting old caches");
   }
